54ac1f879f45101eec3281c8be394d1dfaab16eb
kalomoio <i.kalomoiris@afse.eu>
2019-03-25 10:53:26 +0200

    more fixes

--- app/src/main/java/com/kalom/UnipiTouristicApp/ShowActivity.java
+++ app/src/main/java/com/kalom/UnipiTouristicApp/ShowActivity.java
@@ -2,24 +2,35 @@ package com.kalom.UnipiTouristicApp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class ShowActivity extends AppCompatActivity {
 
+    private static final int CONTENT_VIEW_ID = 10101010;
     private PositionModel pointOfInterest;
     private TextView title;
     private TextView description;
     private TextView category;
+    private ImageView image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show);
+
         title = findViewById(R.id.titleView);
         description = findViewById(R.id.descriptionView);
         category = findViewById(R.id.categoryView);
+
         pointOfInterest = (PositionModel) getIntent().getSerializableExtra("pointOfInterest");
         setViewContent(pointOfInterest);
+
+        FrameLayout frame = new FrameLayout(this);
+        frame.setId(CONTENT_VIEW_ID);
+        setContentView(frame, new FrameLayout.LayoutParams(
+                FrameLayout.LayoutParams.MATCH_PARENT, FrameLayout.LayoutParams.MATCH_PARENT));
 
     }
 
